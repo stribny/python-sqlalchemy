3275391a9b6db7236a502e854085ea9ccf903db7
Petr Stribny <petrstribny@gmail.com>
2020-02-02 17:13:24 +0100

    Add alembic

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ app = typer.Typer()
 
 @app.command()
 def init():
-    ...    
+    ...
+
+
+@app.command()
+def gen_migrations():
+    # https://alembic.sqlalchemy.org/en/latest/api/commands.html
+    ...
 
 
 if __name__ == "__main__":
